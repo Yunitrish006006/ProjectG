993a2e157b92ea42a1ca4af0b66422196ef25a78
yunitrish <yunthomas006@gmail.com>
2025-06-07 19:49:39 +0800

    Add LedController class for RGB LED control and refactor main logic

---
+++ src/LedController.cpp
@@ -0,0 +1,35 @@
+#include "LedController.h"
+
+LedController::LedController(int neopixelPin, int numPixels, int redPin, int greenPin, int bluePin)
+  : _neopixelPin(neopixelPin), _numPixels(numPixels),
+    _redPin(redPin), _greenPin(greenPin), _bluePin(bluePin),
+    _currentColorIndex(0),
+    _pixels(numPixels, neopixelPin, NEO_GRB + NEO_KHZ800) {
+}
+
+void LedController::begin() {
+  _pixels.begin();
+  _pixels.setBrightness(50);
+  _pixels.show();
+  
+  ledcSetup(RED_CHANNEL, PWM_FREQ, PWM_RESOLUTION);
+  ledcSetup(GREEN_CHANNEL, PWM_FREQ, PWM_RESOLUTION);
+  ledcSetup(BLUE_CHANNEL, PWM_FREQ, PWM_RESOLUTION);
+  
+  ledcAttachPin(_redPin, RED_CHANNEL);
+  ledcAttachPin(_greenPin, GREEN_CHANNEL);
+  ledcAttachPin(_bluePin, BLUE_CHANNEL);
+}
+
+void LedController::setColor(uint8_t r, uint8_t g, uint8_t b) {
+  uint32_t neoPixelColor = _pixels.Color(r, g, b);
+  _pixels.setPixelColor(0, neoPixelColor);
+  _pixels.show();
+  setRGBColor(r, g, b);
+}
+
+void LedController::setRGBColor(int r, int g, int b) {
+  ledcWrite(RED_CHANNEL, r);
+  ledcWrite(GREEN_CHANNEL, g);
+  ledcWrite(BLUE_CHANNEL, b);
+}
